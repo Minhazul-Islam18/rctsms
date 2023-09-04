4251bbdfceebb89ecf093178df5b20f1d52f7d30
Minhazul-Islam18 <minhaz019166@email.com>
2023-09-04 21:13:58 +0600

    class

--- app/Livewire/Frontend/TeachingPermissionPageComponent.php
+++ app/Livewire/Frontend/TeachingPermissionPageComponent.php
@@ -2,14 +2,34 @@
 
 namespace App\Livewire\Frontend;
 
-use App\Models\TeachingPermission;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use App\Models\TeachingPermission;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Response;
 
 class TeachingPermissionPageComponent extends Component
 {
     #[Layout('livewire.frontend.layouts.common')]
-
+    public function downloadFile($filename)
+    {
+        $filePath = storage_path('app/public/' . $filename);
+        if (file_exists($filePath)) {
+            $fileContents = Storage::disk('public')->get($filename);
+            return Response::stream(
+                function () use ($fileContents) {
+                    echo $fileContents;
+                },
+                200,
+                [
+                    'Content-Type' => 'application/octet-stream',
+                    'Content-Disposition' => 'attachment; filename="' . basename($filePath) . '"',
+                ]
+            );
+        } else {
+            abort(404);
+        }
+    }
     public function render()
     {
         $permissions = TeachingPermission::all();
